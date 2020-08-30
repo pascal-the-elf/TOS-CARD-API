2ee8ae2c42184755b4eca1e4ae1fa9307a3484dd
JacobLinCool <jacoblincool@gmail.com>
2020-08-30 16:26:19 +0800

    1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# TOS-CARD-API
+# 神魔之塔卡片 API
+非官方卡片資訊 API
+
+## 卡片資訊項目
+提供的卡片資訊項目包含
+- 卡片 ID
+- 卡片名稱
+- 卡片屬性
+- 卡片種族
+- 卡片稀有度
+- 卡片空間
+- 卡片系列名稱
+- 卡片初等數值
+- 卡片滿等數值
+- 卡片主動技能
+- 卡片隊長技能
+- 卡片隊伍技能
+- 卡片額外資訊
+- 卡片故事
+- 卡片各等級數值表
+
+## API 請求方式
+1. 使用 tos_card.js 請求
+```html
+<script src="https://cdn.jsdelivr.net/gh/pascal-the-elf/TOS-CARD-API@latest/js/tos_card.min.js"></script>
+
+<script>
+    var card = new tos_card();
+
+    card.get(2454).then(card_data => {
+        // card_data: 卡片資料
+        document.body.innerHTML += `<pre>${card_data}</pre>`;
+    });
+</script>
+```
+
+2. 使用 HTTP GET Request 請求
+```javascript
+fetch("https://tos-card-api.pascaltheelf.workers.dev/?id=2454").then(r=>r.json()).then(resp => {
+    // resp: API 回傳資料，包含 {info, data}
+    document.body.innerHTML += `<pre>${resp.data}</pre>`;
+});
+```
---
+++ js/tos_card.js
@@ -0,0 +1,66 @@
+function tos_card() {
+    var self = this;
+    self.get = async function(id=0, options=self.settings.options) {
+        if(!id) {
+            console.error("沒有提供卡片ID");
+            return;
+        }
+        if(self.settings.cache) {
+            console.log("已啟用快取功能，查詢快取資料");
+            let cache = await self.cache(id, options);
+            if(cache) return cache;
+        }
+        console.log("開始向 API 請求資料");
+        var response = await fetch("https://tos-card-api.pascaltheelf.workers.dev/?id="+id+self.build_options(options)).then(r=>r.json());
+        if(response.info.success) {
+            console.log("成功向 API 請求資料", response);
+            if(self.settings.cache) {
+                console.log("已啟用快取功能，寫入快取資料");
+                var storage = JSON.parse(localStorage[self.settings.cache_name]);
+                storage[id] = {
+                    options: Object.values(options).map(x=>x?1:0).join("").substr(1),
+                    time: Date.now(),
+                    card: response.data
+                };
+                localStorage[self.settings.cache_name] = JSON.stringify(storage);
+                console.log("已寫入快取資料", storage);
+            }
+            console.log("回傳卡片資料");
+            return response.data;
+        }
+    };
+    self.cache = async function(id=0, options=self.settings.options) {
+        if(!id) {
+            console.error("沒有提供卡片ID");
+            return;
+        }
+        if(!localStorage[self.settings.cache_name]) localStorage[self.settings.cache_name] = `{}`;
+        var storage = JSON.parse(localStorage[self.settings.cache_name]);
+        var cached = storage[id];
+        if(cached && cached.options === Object.values(options).map(x=>x?1:0).join("").substr(1) && cached.time + self.settings.cache_time >= Date.now()) {
+            console.log("已取得符合的快取資料，使用快取資料");
+            return cached.card;
+        }
+        console.log("未取得符合的快取資料，使用 API 請求資料");
+        return null;
+    };
+    self.build_options = function(options) {
+        var built = "";
+        Object.entries(options).forEach(item => {
+            built += item[1] ? `&${item[0]}` : "";
+        });
+        return built;
+    };
+    self.settings = {
+        cache: true,
+        cache_time: 12*60*60*1000,
+        cache_name: "tos_card_cache",
+        options: {
+            minify: false,
+            noskills: false,
+            noinfo: false,
+            nostory: false,
+            notable: false
+        }
+    };
+}
